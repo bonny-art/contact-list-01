408d78e880b3bf5955e1e121a86aa32d45db7822
bonny-art <svitlana.otenko@gmail.com>
2023-10-29 10:40:32 +0200

    adds form input det data

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+import { formEl } from "./refs";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formEl.addEventListener("submit", onFormInput);
+
+function onFormInput(e) {
+  e.preventDefault();
+  //   const { name, number, email } = e.curentTarget.elements;
+  //   console.log(name);
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  e.target.reset();
+}
